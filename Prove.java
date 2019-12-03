aa486355790f35f0b11863946951d91ea3ec810b
saramazov <andrea-libero@hotmail.it>
2019-12-03 16:56:09 +0100

    Initial commit.

---
+++ Prove.java
@@ -0,0 +1,32 @@
+package esercizi;
+
+import java.util.HashSet;
+import java.util.HashMap;
+
+
+public class Prove {
+
+	static HashMap<Integer, String> map = new HashMap<>();
+	//static Collection c = new Collection<Integer>();
+	static HashSet<String> set = new HashSet<String>();
+	
+	public static void main(String[] args) {
+		System.out.println(map.hashCode());
+		map.put(1,"a");
+		System.out.println(map.hashCode());
+		map.put(11,"b");
+		System.out.println(map.hashCode());
+		map.put(11,"c");
+		System.out.println(map.hashCode());
+		map.put(11,"d");
+		System.out.println(map.hashCode());
+		map.put(11,"e");
+		System.out.println(map.hashCode());
+				//System.out.println(map.keySet());
+		//System.out.println(map.values());
+		set.add("bfdfdsfrfesfewfewfefewfetwertefge");
+		System.out.println(set.hashCode());
+		
+	}
+
+}
